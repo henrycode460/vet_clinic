9958c2c4ab913c861684140d978b0ce436709354
Henry <dokinruss@gmail.com>
2022-08-24 13:20:43 +0000

    CREATE THE OWNERS AND SPECIES TABLES AND ADD NECESSARY DATA INTO THEM

--- schema.sql
+++ schema.sql
@@ -9,3 +9,29 @@ CREATE TABLE animals (
     weigth_kg DECIMAL,
     species VARCHAR(50)
 );
+
+CREATE TABLE owners (
+   id SERIAL PRIMARY KEY,
+   full_name VARCHAR(50) NOT NULL,
+   age INT NOT NULL
+   
+);
+
+CREATE TABLE species (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(50) NOT NULL
+  
+);
+
+CREATE SEQUENCE my_serials AS integer START 1 OWNED BY animals.id;
+ALTER TABLE animals ALTER COLUMN id SET DEFAULT setval('my_serials');
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT fk_constraint FOREIGN KEY (species_id) REFERENCES species (id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
