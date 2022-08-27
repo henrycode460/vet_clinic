7c5772bd74d13b15e0edb9a00a47de30f9f23f5b
Henry <dokinruss@gmail.com>
2022-08-27 15:05:31 +0000

    update the query file

--- queries.sql
+++ queries.sql
@@ -6,18 +6,25 @@ JOIN animals ON vet_id = (SELECT id FROM vets WHERE vets.name = 'Vet William Tat
 AND animals.id = visits.animals_id
 ORDER BY date_of_visit DESC;
 
--- How many different animals did Stephanie Mendez see?
-SELECT vets.name , animals.name AS Animals_Name
+-- What animal has the most visits to vets?
+SELECT animals.name AS Animals, COUNT(animals_id)
 FROM visits
-JOIN vets ON vet_id = visits.vet_id
-JOIN animals ON vet_id = (SELECT id FROM vets WHERE vets.name = 'Vet Stephanie Mendez')
-AND animals.id = visits.animals_id;
+JOIN animals ON animals.id = visits.animals_id
+GROUP BY animals.name
+ORDER BY COUNT(animals_id) DESC;
 
 -- List all vets and their specialties, including vets with no specialties.
 SELECT vets.name,  species.name AS specialties
 FROM  specializations
 JOIN vets ON vets.id = specializations.vet_id
 JOIN species ON species.id = specializations.species_id;
+
+-- How many different animals did Stephanie Mendez see?
+SELECT vets.name , animals.name AS Animals_Name
+FROM visits
+JOIN vets ON vet_id = visits.vet_id
+JOIN animals ON vet_id = (SELECT id FROM vets WHERE vets.name = 'Vet Stephanie Mendez')
+AND animals.id = visits.animals_id;
 
 -- List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020.
 SELECT vets.name, animals.name AS Animals_Name, date_of_visit
@@ -27,12 +34,6 @@ JOIN vets ON vets.id = visits.vet_id
 AND vets.name = 'Vet Stephanie Mendez'
 AND date_of_visit BETWEEN '2020-04-01' AND '2020-08-30';
 
--- What animal has the most visits to vets?
-SELECT animals.name AS Animals, COUNT(animals_id)
-FROM visits
-JOIN animals ON animals.id = visits.animals_id
-GROUP BY animals.name
-ORDER BY COUNT(animals_id) DESC;
 
 
 -- Who was Maisy Smith's first visit?
